90da9b1d4888cbe32ca22e57c8c5ade043241847
Jamie897 <jamiedanielle456@gmail.com>
2024-04-17 19:13:39 -0500

    Fixing ;inter Errors

---
+++ SRC/routes.js/root.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-nested-ternary */
+import { Outlet, NavLink, useNavigation } from 'react-router-dom';
+
+export default function Root() {
+  const navigation = useNavigation();
+  return (
+    <>
+      <div id="sidebar">
+        <h1>Space Travelers&apos; Hub</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) => (isActive
+                  ? 'active'
+                  : isPending
+                    ? 'pending'
+                    : '')}
+              >
+                Rockets
+
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="missions/"
+                className={({ isActive, isPending }) => (isActive
+                  ? 'active'
+                  : isPending
+                    ? 'pending'
+                    : '')}
+              >
+                Missions
+
+              </NavLink>
+            </li>
+            <li className="separator" />
+            <li>
+              <NavLink
+                to="myprofile/"
+                className={({ isActive, isPending }) => (isActive
+                  ? 'active'
+                  : isPending
+                    ? 'pending'
+                    : '')}
+              >
+                My Profile
+
+              </NavLink>
+            </li>
+
+          </ul>
+        </nav>
+      </div>
+      <div id="detail" className={navigation.state === 'loading' ? 'loading' : ''}>
+        <Outlet />
+      </div>
+    </>
+  );
+}
